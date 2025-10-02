254f4502dc4fadaf24bbda435235ae8f1a012531
Raja Singh, Manoj <MRajasingh@ashleyfurniture.com>
2025-10-02 16:03:07 -0400

    configure server port

--- modern.config.ts
+++ modern.config.ts
@@ -2,6 +2,9 @@ import { appTools, defineConfig } from '@modern-js/app-tools';
 
 // https://modernjs.dev/en/configure/app/usage
 export default defineConfig({
+  server: {
+    port: 3000,
+  },
   runtime: {
     router: true,
   },
